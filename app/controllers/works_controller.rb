0ebe7c6818b3d3a3cdd0fe4bb75e441ccff64543
erikyoung <erik.young@rmit.edu.vn>
2018-07-16 23:50:55 +0700

    working on debugging the contact page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,20 @@ class WorksController < ApplicationController
 	def index
 		@works = Work.all
 	end
+
+	def new
+		@work = Work.new
+	end
+
+	def create
+		Work.create(work_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def work_params
+		params.require(:work).permit(:name, :message, :email, :address)
+	end
+
 end
