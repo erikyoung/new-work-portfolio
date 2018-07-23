1c33ed4d13a3f14d9b1274073538d2d07695c22e
erikyoung <erik.young@rmit.edu.vn>
2018-07-23 23:31:12 +0700

    working on receiving emails

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
  get 'portfolio', to: 'works#index'
  get 'contact', to: 'works#contact'
  get 'resume', to: 'works#resume'
+ get 'contact-me', to: 'messages#new', as: 'new_message'
+ post 'contact-me', to: 'messages#create', as: 'create_message'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
