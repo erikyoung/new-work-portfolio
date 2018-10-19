ddc6738ed0910cc52dd325951264d4577dbf8218
erikyoung <erik.young@rmit.edu.vn>
2018-10-19 13:03:13 +0700

    straightened out hw tracker page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,7 +47,13 @@
 
             $("#app-title-mirror").css('transform', 'translate(0px,' + scrollval/2 + '%)'); 
             $('#spotify-cover').css('transform', 'translate(0px,' + scrollval/2 + '%)');
+            
         }); 
+
+        $(".hw-top-box").animate({
+                scrollTop: $(document).height()
+                }, 600);
+                return false;
  
 
   }); 
